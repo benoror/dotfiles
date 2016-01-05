5860e92e92292dc5caef17a3d7f06561564035f0
Benji Orozco <benoror@gmail.com>
2016-01-05 17:43:16 -0600

    cleanup after a while :)

--- hostednetwork.bat
+++
@@ -1,3 +0,0 @@
-netsh wlan stop hostednetwork
-netsh wlan set hostednetwork mode=allow ssid=lenovonetwork key=1234567890
-netsh wlan start hostednetwork
--- iconsets/monochrome tray/cp.sh
+++
@@ -1,1 +0,0 @@
-sudo cp humanityforparcellite/edit-paste.svg /usr/share/icons/gnome-brave/24x24/actions/
